4a90e26bf1de1c9c0a667598e59f95ffb440c8df
Shanker joshi <shankerjoshi743@gmail.com>
2024-06-02 07:09:23 +0530

    add user and video model with changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,8 @@
 import mongoose, { Schema } from "mongoose";
 
+import bcrypt from "bcrypt"
+import { jwt } from "jsonwebtoken";
+
 const userSchema = new Schema({
   username: {
     type: String,
@@ -44,4 +47,25 @@ const userSchema = new Schema({
   }
 }, { timestamps: true })
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next()
+
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+})
+
+userSchema.method.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.method.generateAccessToken = function () {
+  return jwt.sign({
+    _id: this._id,
+  },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expireIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
 export const User = mongoose.model("User", userSchema)
